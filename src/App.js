caa5110782fa8b3eb9a3dbd54763619bb89edbf6
Laura-html <lrichards023@gmail.com>
2026-07-05 06:04:42 +0100

    created search function and form for dictionary

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">
+        <header className="app-header">
           <img src={logo} className="app-logo img-fluid" alt="logo" />
         </header>
 
