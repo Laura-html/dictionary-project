785c4a5d72f21e44ce4074178f509132b8d84f44
Laura-html <lrichards023@gmail.com>
2026-07-05 05:06:17 +0100

    added footer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ export default function App() {
       <button type="button" class="btn btn-primary">
         Primary
       </button>
+      <footer>
+        {" "}
+        Created by Laura, open-sourced on{" "}
+        <a href="https://github.com/Laura-html/dictionary-project">
+          Github
+        </a>{" "}
+        and hosted on{" "}
+        <a href="https://dictionary-app-3.netlify.app/"> Netlify</a>.{" "}
+      </footer>
     </div>
   );
 }
