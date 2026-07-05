9abacdf2cc335dc38e7a2ae124e11fcb951dc637
Laura-html <lrichards023@gmail.com>
2026-07-05 05:32:07 +0100

    added dictionary page with own css page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello World
-      <button type="button" class="btn btn-primary">
-        Primary
-      </button>
-      <footer>
-        {" "}
-        Created by Laura, open-sourced on{" "}
-        <a href="https://github.com/Laura-html/dictionary-project">
-          Github
-        </a>{" "}
-        and hosted on{" "}
-        <a href="https://dictionary-app-3.netlify.app/"> Netlify</a>.{" "}
-      </footer>
+      <div className="container">
+        <header className="App-header"></header>
+
+        <main>
+          <Dictionary />
+        </main>
+
+        <footer>
+          {" "}
+          Created by Laura, open-sourced on{" "}
+          <a href="https://github.com/Laura-html/dictionary-project">
+            Github
+          </a>{" "}
+          and hosted on{" "}
+          <a href="https://dictionary-app-3.netlify.app/"> Netlify</a>.{" "}
+        </footer>
+      </div>
     </div>
   );
 }
