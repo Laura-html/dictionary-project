ded7f773e9b36dae98bd132c150d4c3b9e7f1c2a
Laura-html <lrichards023@gmail.com>
2026-07-06 18:43:41 +0100

    trying to fix synonyms list

--- src/Meaning.js
+++ src/Meaning.js
@@ -3,18 +3,16 @@ import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
-    <div className="meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>;
+    <div className="Meaning">
+      <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
             <p>
-              <strong>Definition:</strong>
-              {definition.definition}
+              <strong>Definition:</strong> {definition.definition}
               <br />
               <strong>Example:</strong>
               <em>{definition.example}</em>
-
               <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
