acc212ac03d3bffc48c874eed0b5aef95edee699
Laura-html <lrichards023@gmail.com>
2026-07-07 03:27:53 +0100

    Added css to main page and set default term

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
         </header>
 
         <main>
-          <Dictionary />
+          <Dictionary defaultKeyword="sunset" />
         </main>
 
         <footer className="app-footer">
